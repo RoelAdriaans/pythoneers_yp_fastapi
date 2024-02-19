e3632f7e00bd714d559d2a984fa78046f671715a
Roel Adriaans <roel@adriaans.org>
2024-02-19 13:11:08 +0100

    Add a function

--- helloworld/main.py
+++ helloworld/main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 async def root(args):    
     return {"message": "Hello World"}
+
+@app.get("/about")
+async def about(args):    
+    return {"message": "About the project"}  
